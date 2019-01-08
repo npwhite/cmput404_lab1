edc70fe0a8553ca123cc02929b01cf44173434c5
Nicholas Whitefield <whitefie@ualberta.ca>
2019-01-08 11:26:52 -0700

    couple changes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ print(requests.__version__)
 
 r = requests.get("http://google.com/")
 print(r.status_code)
+#print(r.headers)
 
 github_url = "https://raw.githubusercontent.com/npwhite/cmput404_lab1/master/main.py"
 r = requests.get(github_url)
