f6ae9b2afbc3eca14a25d168d3e17601d1269d2c
Nicholas Whitefield <whitefie@ualberta.ca>
2019-01-08 10:47:53 -0700

    Q1 - Q7 Done

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import requests
 
 print(requests.__version__)
 
-r = requests.get(google)
-print(r.satus_code)
-print(r.headers)
+r = requests.get("http://google.com/")
+print(r.status_code)
+#print(r.headers)
 
-girhub_url = ".../main.py" # fill in ...'s based on your github path to main.py after pushing
-r = requests.get(github_url)
-print(r.text)
+#girhub_url = ".../main.py" # fill in ...'s based on your github path to main.py after pushing
+#r = requests.get(github_url)
+#print(r.text)
 
